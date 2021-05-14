8aa092bb84bbc2b32024d5c5e821b5e0e67a916c
aguerrre <agustinguerrero.reyes@gmail.com>
2021-05-14 13:13:31 +0200

    Pinball finalizado, falta menú

--- src/fpdualeveris/FPDual.java
+++ src/fpdualeveris/FPDual.java
@@ -1,9 +1,69 @@
 package fpdualeveris;
 
-public class FPDual {
+import java.util.Scanner;
 
+import pinball.Pinball;
+
+/**
+ * Desafío 2 Java
+ * 
+ * @author aguerrre
+ *
+ */
+
+public class FPDual {
+	/**
+	 * Objeto Scanner para usar durante la partida
+	 */
+	private static final Scanner SC = new Scanner(System.in);
+
+	/**
+	 * Método principal de la aplicación
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		
+		initGame();
+	}
+
+	/**
+	 * Método que inicia el juego, creando un objeto de la clase Pinball
+	 */
+	private static void initGame() {
+		Pinball p = new Pinball();
+		while (p.getBall() != 0) {
+			//Lanza una bola nueva
+			p.launchBall();
+			//Juega la partida mientras no haya un fallo en la misma
+			while (!p.isFail()) {
+				p.playGame(sideChoosen());
+			}
+		}
+		//Finaliza la partida y muestra la puntuación obtenida
+		p.finishGame();
+	}
+
+	/**
+	 * Método que devuelve 0 / 1 dependiendo de la tecla pulsada
+	 * @return int optionInt
+	 */
+	private static int sideChoosen() {
+		// dos variables para la opción que se elige
+		String optionStr = "";
+		int optionInt = 0;
+		//Se seguirá manteniendo a la espera hasta que se pulse la tecla correcta, se trata también con excepciones.
+		while (!(optionStr.equalsIgnoreCase("z") || optionStr.equalsIgnoreCase("m"))) {
+			try {
+				optionStr = SC.nextLine();
+			} catch (Exception e) {
+				System.out.println(e.toString());
+				optionStr = "";
+			}
+		}
+		//Dependiendo de la tecla pulsada se asignará 0 / 1
+		optionInt = optionStr.equalsIgnoreCase("z") ? 0 : 1;
+
+		return optionInt;
 	}
 
 }
--- src/pinball/Pinball.java
+++ src/pinball/Pinball.java
@@ -1,5 +1,173 @@
 package pinball;
 
+import java.util.Random;
+
+/**
+ * Desafío 2 Java
+ * 
+ * @author aguerrre
+ *
+ */
+
 public class Pinball {
+	/**
+	 * Objeto StringBuilder para mostrar los mensajes por consola
+	 */
+	private static final StringBuilder STR = new StringBuilder();
+	/**
+	 * Variable de clase que muestra hacia que lado se va la bola durante el juego. Si es 0 es izquierda, si es 1 es derecha
+	 */
+	private static int side;
+	/**
+	 * Objeto Random para crear números aleatorios
+	 */
+	private static final Random r = new Random();
+	/**
+	 * Atributos: número de bolas por partida y booleano que indica si se ha producido un fallo o no
+	 */
+	private int ball;
+	private boolean fail;
+	private long pointsScored;
+
+	/**
+	 * Constructor sin parámetros, pero que inicia las bolas disponibles a 3 y
+	 */
+	public Pinball() {
+		super();
+		this.ball = 3;
+		this.pointsScored = 0;
+	}
+
+	/**
+	 * @return the side
+	 */
+	public static int getSide() {
+		return side;
+	}
+
+	/**
+	 * @param side
+	 *            the side to set
+	 */
+	public static void setSide(int side) {
+		Pinball.side = side;
+	}
+
+	/**
+	 * @return the r
+	 */
+	public static Random getR() {
+		return r;
+	}
+
+	/**
+	 * @return the ball
+	 */
+	public int getBall() {
+		return ball;
+	}
+
+	/**
+	 * @param ball
+	 *            the ball to set
+	 */
+	public void setBall(int ball) {
+		this.ball = ball;
+	}
+
+	/**
+	 * @return the fail
+	 */
+	public boolean isFail() {
+		return fail;
+	}
+
+	/**
+	 * @param fail
+	 *            the fail to set
+	 */
+	public void setFail(boolean fail) {
+		this.fail = fail;
+	}
+
+	/**
+	 * @return the pointsScored
+	 */
+	public long getPointsScored() {
+		return pointsScored;
+	}
+
+	/**
+	 * @param pointsScored
+	 *            the pointsScored to set
+	 */
+	public void setPointsScored(long pointsScored) {
+		this.pointsScored = pointsScored;
+	}
+
+	/**
+	 * Método para lanzar una nueva bola
+	 */
+	public void launchBall() {
+		System.out.println("Se ha lanzado una nueva bola.");
+		//Se resta una bola al total de bolas disponibles
+		this.setBall(this.getBall() - 1);
+		//Se reinicia la variable fail a false
+		this.setFail(Boolean.FALSE);
+		//Se obtiene el primer lado aleatorio hacia el que va la bola
+		getRandomSide();
+	}
+
+	/**
+	 * Método para jugar cada bola tirada
+	 * @param sideChoosen
+	 */
+	public void playGame(int sideChoosen) {
+		STR.setLength(0);
+		STR.append("Puntuación de la jugada: ").append(getPointsMove());
+		System.out.println(STR);
+		//Se suman al total los puntos de la jugada
+		this.pointsScored += getPointsMove();
+		// Si el lado elegido es el mismo que el aleatorio, obtiene un nuevo lado aleatorio, si no, alerta del fallo y cambia fail a true
+		if (Pinball.side == sideChoosen) {
+			getRandomSide();
+		} else {
+			System.out.println("¡¡Fallo!!");
+			this.setFail(Boolean.TRUE);
+		}
+	}
+	
+	/**
+	 * Método para obtener los puntos de una jugada individual
+	 * @return pointsMove
+	 */
+	private int getPointsMove() {
+		int pointsMove = r.nextInt(100 + 1) * 100;
+		return pointsMove;
+	}
+
+	/**
+	 * Método para obtener hacia qué lado se va la bola
+	 */
+	private void getRandomSide() {
+		
+		int randomSide = r.nextInt(1 + 1);
+		if (randomSide == 0) {
+			System.out.println("La bola va hacia la izquierda...");
+		} else {
+			System.out.println("La bola va hacia la derecha...");
+		}
+		//Se cambia la variable de clase side
+		Pinball.setSide(randomSide);
+	}
+
+	/**
+	 * Método que finaliza el juego y muestra la puntuación total
+	 */
+	public void finishGame() {
+		STR.setLength(0);
+		STR.append("Puntuación de la partida: ").append(this.getPointsScored());
+		System.out.println(STR);
+	}
 
 }
